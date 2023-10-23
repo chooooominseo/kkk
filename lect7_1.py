0caa3da2497096229803a4ebc8be95cba79473f9
cho minseo <jms529411@gmail.com>
2023-10-23 17:53:00 +0900

    first commit

---
+++ lect7_1.py
@@ -0,0 +1,70 @@
+"""
+import os
+
+fp = "temp.txt"
+
+if os.path.exists(fp) :
+    f = open(fp,"r")
+    for line in f :
+        print(line)
+else :
+    f = open(fp, "w")
+    for i in range(100):
+        f.write(str(i) + "\n")
+        
+f.close()
+"""
+"""
+import os
+fp = "new.txt"
+
+os.remove(fp, "w")
+print("complete")
+"""
+"""
+def dir_print(p) :
+	files = os.listdir(p)
+	for f in files :
+		print(f)
+  
+fp = "new.txt"
+
+f = open(fp,"w")
+f.close()
+
+dir_print("./")
+
+os.remove(fp)
+print("_______________\n\n")
+dir_print("._")
+"""
+"""
+# 파일명 변경
+import os
+
+fp = "new.txt"
+
+f = open(fp,"w")
+f.close()
+
+os.rename(fp, "new.txt")
+print("complete")
+"""
+
+# 재변경 예외처리
+
+import os
+
+fp = "new.txt"
+tp = "new1.txt"
+
+f = open(fp,"w")
+f.close()
+
+if os.path.exists(tp) :
+    print("exist, same name file")
+    os.remove(tp)
+    
+else :
+    os.rename(fp, "new1.txt")
+    print("complete")
